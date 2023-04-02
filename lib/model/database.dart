bff1e2ad1f8646c6c5b5015c8352cd09cdfd1d0b
Eva_Kristiana <keva2532@gmail.com>
2023-04-02 16:06:56 +0000

    pegawai pasien

---
+++ lib/model/database.dart
@@ -0,0 +1,39 @@
+List<Map> pasien = [
+  {
+    'id': 01,
+    'noRm': '12345',
+    'nama': 'Cahyo W',
+    'tanggalLahir': '20 Agustus 2000',
+    'nomorTelepon': '081957392994',
+    'alamat': 'Klaten',
+  },
+  {
+    'id': 02,
+    'noRm': '12344',
+    'nama': 'Agus P',
+    'tanggalLahir': '12 Januari 2002',
+    'nomorTelepon': '085678528309',
+    'alamat': 'Ngawi',
+  },
+];
+
+List<Map> pegawai = [
+  {
+    'id': 01,
+    'nip': '12333',
+    'nama': 'Joko Susanto',
+    'tanggalLahir': '04 Maret 1982',
+    'nomorTelepon': '08973819773',
+    'email': 'jokosus@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 02,
+    'nip': '12233',
+    'nama': 'Hayu Pintasari',
+    'tanggalLahir': '18 Mei 1999',
+    'nomorTelepon': '08752728922',
+    'email': 'hayyp@gmail.com',
+    'password': '456',
+  },
+];
